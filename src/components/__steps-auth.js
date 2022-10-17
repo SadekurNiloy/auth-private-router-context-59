5daf970de0eb6db77b307911a8a96d58e816f5ab
sadik <sadikniloy360@gmail.com>
2022-10-17 08:40:27 +0600

    some file added

---
+++ src/components/__steps-auth.js
@@ -0,0 +1,10 @@
+/* 
+--------------------
+    Auth SetUp 
+--------------------
+1. create firebase project
+2. enbale web
+3. enable email/password and google 
+4. create firebase-init-config.js and set initaillized firebase code 
+5. export app from firebase-init-config.js
+*/
---
+++ src/components/firebase/firebase-init-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVtQI0ywGA7eRClGL9fccdDERuS5iEbxY",
+  authDomain: "ema-john-routes-59.firebaseapp.com",
+  projectId: "ema-john-routes-59",
+  storageBucket: "ema-john-routes-59.appspot.com",
+  messagingSenderId: "738129416753",
+  appId: "1:738129416753:web:8f2211be2e462e4a0a3b10",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
